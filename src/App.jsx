0cff11cedf64ebeebda1a446fa0c79d102e33f73
LJBL22 <ljbl2208@gmail.com>
2024-01-08 19:38:50 +0800

    feat: createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import Menu from './Pages/menu';
-import Header from './components/Header';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Menu from './Pages/Menu';
+import Quiz from './Pages/Quiz';
+import Result from './Pages/Result';
+import Layout from './Pages/Layout';
 
 const App = () => {
-  return (
-    <>
-      <div className='container'>
-        <Header />
-        <Menu />
-      </div>
-    </>
-  );
+  const router = createBrowserRouter([
+    //之前這裡誤刪、沒放進 [] 會跳錯  "You must provide a non-empty routes array to createRouter"；官方文件也是有用 [] 包覆
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        //outlet
+        {
+          path: '',
+          element: <Menu />,
+        },
+        {
+          path: 'quiz',
+          element: <Quiz />,
+        },
+        {
+          path: 'result',
+          element: <Result />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;
