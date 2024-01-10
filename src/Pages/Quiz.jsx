3c07793e11b96a6ce12c35fb4cc9a9b8fea56695
LJBL22 <ljbl2208@gmail.com>
2024-01-10 23:43:08 +0800

    bug: to be fixed

--- src/Pages/Quiz.jsx
+++ src/Pages/Quiz.jsx
@@ -1,12 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '../Components/Button';
 import { useEffect, useState } from 'react';
-import { setQuestions } from '../store';
+import { setQuestions, chooseAnswer } from '../store';
 
 const Quiz = () => {
   // Access states
   const { quizzes, quizTitle } = useSelector((state) => state.menu);
-  const { questions, index } = useSelector((state) => state.quiz);
+  const { questions, index, selectedAnswer } = useSelector(
+    (state) => state.quiz
+  );
   const selectedData = quizzes.find((item) => item.title === quizTitle);
   const dispatch = useDispatch();
   const [isSubmitted, setIsSubmitted] = useState(false);
@@ -18,16 +20,28 @@ const Quiz = () => {
   const options = selectedData.questions[index].options;
   const answer = selectedData.questions[index].answer;
 
+  // Warning: Cannot update a component (`Header`) while rendering a different component (`Quiz`). To locate the bad setState() call inside `Quiz`, follow the stack trace as described in https://reactjs.org/link/setstate-in-render
   useEffect(() => {
     dispatch(setQuestions(selectedData?.questions));
   }, [quizTitle, dispatch, selectedData]);
 
+  // console.js:213 Warning: Expected `onClick` listener to be a function, instead got a value of `object` type.
+  // onClick 在一個 option 即選定答案、更新 state
+  // const setAnswer = (option) => dispatch(chooseAnswer(option));
+
+  const handleClick = () => {
+    // 對答案 （取 answer、取 state: selectedAnswer)
+    // console.log(selectedAnswer);
+    // 更改 button 字樣
+    setIsSubmitted(!isSubmitted);
+  };
   const renderedOptions = options.map((option, i) => {
     return (
-      <div key={option}>
+      <button key={option}>
+        {/* <button key={option} onClick={setAnswer(option)}> */}
         <span>{String.fromCharCode(65 + i)}</span>
         <span>{option}</span>
-      </div>
+      </button>
     );
   });
   return (
@@ -42,7 +56,7 @@ const Quiz = () => {
       {renderedOptions}
       <Button
         content={isSubmitted ? 'Next Question' : 'Submit Answer'}
-        onClick={() => setIsSubmitted(!isSubmitted)}
+        onClick={handleClick}
       />
     </div>
   );
