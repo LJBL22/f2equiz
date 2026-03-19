05b345f569e9223c9794c617c0fb9cce30a64653
LJBL22 <ljbl2208@gmail.com>
2026-03-19 13:56:52 +0800

    fix: replace checkbox toggle with div-based switch
    Replaces the hidden input[type=checkbox] in Switch.tsx with a plain div
    and onClick handler to fix the theme toggle not responding correctly.

--- src/Components/Switch.tsx
+++ src/Components/Switch.tsx
@@ -1,45 +1,46 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { setTheme } from '../store';
-import type { RootState, AppDispatch } from '../store';
+import { useDispatch, useSelector } from "react-redux";
+import { setTheme } from "../store";
+import type { RootState, AppDispatch } from "../store";
 
 const Switch = () => {
   const { theme } = useSelector((state: RootState) => state.menu);
   const dispatch = useDispatch<AppDispatch>();
 
   const handleChange = () => {
-    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    const newTheme = theme === "dark" ? "light" : "dark";
     dispatch(setTheme(newTheme));
-    localStorage.setItem('theme', newTheme);
-    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem("theme", newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
   };
 
   return (
-    <div className='flex flex-row w-auto items-center gap-2'>
-      <div className='w-4 h-4 tablet:w-6 tablet:h-6'>
+    <div className="flex flex-row w-auto items-center gap-2">
+      <div className="w-4 h-4 tablet:w-6 tablet:h-6">
         <img
           src={
-            theme === 'dark' ? './icon-sun-light.svg' : './icon-sun-dark.svg'
+            theme === "dark" ? "./icon-sun-light.svg" : "./icon-sun-dark.svg"
           }
-          alt='icon-sun'
+          alt="icon-sun"
         />
       </div>
-      <div className='w-8 h-5 bg-purple rounded-full relative tablet:w-12 tablet:h-7'>
-        <input
-          type='checkbox'
-          onChange={handleChange}
-          className={`appearance-none w-3 h-3 tablet:w-5 tablet:h-5 bg-pure-white rounded-full absolute inset-y-1 left-1 transition-transform cursor-pointer ${
-            theme === 'dark'
-              ? 'translate-x-3 tablet:translate-x-5'
-              : 'translate-x-0'
+      <div
+        onClick={handleChange}
+        className="w-8 h-5 bg-purple rounded-full relative tablet:w-12 tablet:h-7 cursor-pointer"
+      >
+        <div
+          className={`w-3 h-3 tablet:w-5 tablet:h-5 bg-pure-white rounded-full absolute inset-y-1 left-1 transition-transform pointer-events-none ${
+            theme === "dark"
+              ? "translate-x-3 tablet:translate-x-5"
+              : "translate-x-0"
           }`}
         />
       </div>
-      <div className='w-4 h-4 tablet:w-6 tablet:h-6'>
+      <div className="w-4 h-4 tablet:w-6 tablet:h-6">
         <img
           src={
-            theme === 'dark' ? './icon-moon-light.svg' : './icon-moon-dark.svg'
+            theme === "dark" ? "./icon-moon-light.svg" : "./icon-moon-dark.svg"
           }
-          alt='icon-moon'
+          alt="icon-moon"
         />
       </div>
     </div>
